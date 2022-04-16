72fb6b09b69f12a444ef8b0ab6a707a22f5c593b
Dwopplee <charlesparkinson14@gmail.com>
2022-04-15 23:27:09 -0700

    Start of a progress bar
    may have accidentally broken some main page stuff, will verify later
    will try to implement theme colors and daily reset, as well as possibly
    more informative properties

---
+++ HealthApp/src/CaloriesScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+export default function CaloriesScreen({navigation}) {
+
+    return (
+        <View>
+            <Text>PlaceHolder</Text>
+        </View>
+    );
+}
