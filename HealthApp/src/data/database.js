8fef5b3be490d68e7854a1e71a2e417ec961a2d4
TensorCore <shakerad0109@gmail.com>
2022-04-18 05:07:14 -0700

    Experimental DB

--- HealthApp/src/data/database.js
+++ HealthApp/src/data/database.js
@@ -1,20 +1,76 @@
 import React from 'react';
 import * as SQLite from 'expo-sqlite';
 
-const db = openDatabase();
+const db = openDatabase('db.db');
 
-const getSetting = (setSettingFunc) => {
+const getData = (setDataFunc) => {
     db.transaction(
         tx => {
             tx.executeSql(
-            'select * from setting',
+            'select * from data',
             [],
             (_, { rows: { _array } }) => {
-                setSettingFunc(_array)
+                setDataFunc(_array)
         }
       );
     },
-     (t, error) => { console.log("db error load settings"); console.log(error) },
-     (_t, _success) => { console.log("loaded settings")}
+     (t, error) => { console.log("db error load data"); console.log(error) },
+     (_t, _success) => { console.log("loaded data")}
     );
-} 
+}
+
+const insertData = (water, exercise, calorie, successFunc) => {
+    db.transaction( tx => {
+        tx.executeSql( "insert into data (water, exercise, calorie) values (?, ?, ?)", [water, exercise, calorie]);
+      },
+      (t, error) => { console.log("db error insertData"); console.log(error);},
+      (t, success) => { successFunc() }
+    )
+}
+
+
+const dropDatabaseTablesAsync = async () => {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'drop table data',
+          [],
+          (_, result) => { resolve(result) },
+          (_, error) => { console.log("error dropping data table"); reject(error)
+          }
+        )
+      })
+    })
+  }
+
+  const setupDatabaseAsync = async () => {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+          tx.executeSql(
+            'create table if not exists data (id integer primary key not null, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, water integer, exercise integer, calories integer);'
+          );
+        },
+        (_, error) => { console.log("db error creating tables"); console.log(error); reject(error) },
+        (_, success) => { resolve(success)}
+      )
+    })
+  }
+
+  const setupDataAsync = async () => {
+    return new Promise((resolve, _reject) => {
+      db.transaction( tx => {
+          tx.executeSql( "insert into data (water, exercise, calorie) values (?, ?, ?, ?)", [2, 30, 1000] );
+        },
+        (t, error) => { console.log("db error insertData"); console.log(error); resolve() },
+        (t, success) => { resolve(success)}
+      )
+    })
+  }
+
+  export const database = {
+      getData,
+      insertData,
+      setupDatabaseAsync,
+      dropDatabaseTablesAsync,
+      setupDataAsync,
+  }
