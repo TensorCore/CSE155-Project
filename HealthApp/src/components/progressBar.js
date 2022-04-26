5af5f11e986b031248b545d4ee32213f8111cfb5
Dwopplee <charlesparkinson14@gmail.com>
2022-04-25 17:58:52 -0700

    Fixed progress bar colors
    also remove warning for graphs

--- HealthApp/src/components/progressBar.js
+++ HealthApp/src/components/progressBar.js
@@ -9,7 +9,22 @@ export default function ProgressBar(props) {
     // Make sure progress value is linked to data
     const { data } = useContext(DataContext);
 
+    const { colors } = useTheme();
+
+    const getProgressColor = (val) => {
+        let pcnt = val / props.max;
+        if (pcnt < .34) {
+            return (colors.primary === 'azure' ? '#FAA' : '#A55');
+        } else if (pcnt < .67) {
+            return (colors.primary === 'azure' ? '#FC5' : '#A70');
+        } else {
+            return (colors.primary === 'azure' ? '#AFA' : '#5A5');
+        }
+    }
+
     const [progress, setValue] = useState(0);
+
+    var progressUpdated = false;
 
     // Extract only the value we care about (today's props.name value)
     const getDailyData = (obj) => {
@@ -17,31 +32,24 @@ export default function ProgressBar(props) {
         // Also, getToday() might need some time localization to work
         // if (getToday() === obj.timestamp) {
         if (props.selectedDate === obj.timestamp) {
-            console.log(props.selectedDate === obj.timestamp);
             setValue(obj[props.name]);
-        }
-    }
-
-    const getProgressColor = () => {
-        let progressPerc = progress / props.max;
-        if (progressPerc < .34) {
-            return (colors.primary === 'azure' ? '#FAA' : '#A55');
-        } else if (progressPerc < .67) {
-            return (colors.primary === 'azure' ? '#FA5' : '#A50');
-        } else {
-            return (colors.primary === 'azure' ? '#AFA' : '#5A5');
+            progressUpdated = true;
         }
     }
 
     // Make sure colors are on theme
-    const { colors } = useTheme();
-    const [progressColor, setColor] = useState(getProgressColor());
+    const [progressColor, setColor] = useState(getProgressColor(progress));
 
     useEffect(() => {
-        if (!(typeof data === 'undefined'))
+        if (!(typeof data === 'undefined')) {
+            progressUpdated = false;
             data.map(getDailyData);
-        setColor(getProgressColor());
-    }, [colors, data, props.selectedDate])
+            if (!progressUpdated) {
+                setValue(0);
+            }
+        }
+        setColor(getProgressColor(progress));
+    }, [colors, data, props.selectedDate, progress])
 
     return (
         <View>
