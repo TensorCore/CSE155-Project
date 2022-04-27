c8e201e3b4faec6ced68e0dfeb47142052690625
Dwopplee <charlesparkinson14@gmail.com>
2022-04-26 20:58:35 -0700

    Minor graph updates
    put a graph on every page
    copy pasted the historical data some more

--- HealthApp/src/screens/ExerciseScreen.js
+++ HealthApp/src/screens/ExerciseScreen.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useTheme } from '@react-navigation/native';
 import { View, Text, StyleSheet } from 'react-native';
+import TimeGraph from '../components/timeGraph';
 
 
-export default function ExerciseScreen({navigation}) {
-    const {colors} = useTheme();
+export default function ExerciseScreen({ navigation }) {
+    const { colors } = useTheme();
     useEffect(() => {
-        navigation.setOptions({headerStyle: {backgroundColor: colors.primary}, 
-                               headerTintColor: colors.text, 
-                              });
+        navigation.setOptions({
+            headerStyle: { backgroundColor: colors.primary },
+            headerTintColor: colors.text,
+        });
     }, [colors])
     return (
         <View>
-            <Text>PlaceHolder</Text>
+            <TimeGraph name="exercise" goal={1000} />
         </View>
     );
 }
--- HealthApp/src/screens/WaterScreen.js
+++ HealthApp/src/screens/WaterScreen.js
@@ -2,19 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
 import { useTheme } from '@react-navigation/native';
 import { View, Text, StyleSheet } from 'react-native';
-import {  } from 'react-native';
+import TimeGraph from '../components/timeGraph';
 
-export default function WaterScreen({navigation}) {
-    const {colors} = useTheme();
+export default function WaterScreen({ navigation }) {
+    const { colors } = useTheme();
     useEffect(() => {
-        navigation.setOptions({headerStyle: {backgroundColor: colors.primary}, 
-                               headerTintColor: colors.text, 
-                              });
+        navigation.setOptions({
+            headerStyle: { backgroundColor: colors.primary },
+            headerTintColor: colors.text,
+        });
     }, [colors])
 
     return (
         <View>
-            <Text>PlaceHolder</Text>
+            <TimeGraph name="water" goal={8} />
         </View>
     );
 }
